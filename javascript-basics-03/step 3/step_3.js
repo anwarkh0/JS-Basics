dd00c4cdc79d45621201c8b9403bb538e9bd5169
anwar khawli <anwarkhawle@gmail.com>
2023-09-01 10:22:47 +0300

    step 3

---
+++ javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,13 @@
+let color = document.querySelectorAll(".color")
+let text = document.querySelector("#text")
+for(let i=0; i<color.length; i++){
+    color[i].addEventListener("click", function(event){
+        if (event.target.classList.contains("green"))
+            text.style.color = "green" ;
+        else if (event.target.classList.contains("red"))
+            text.style.color = "red" ;
+        else if (event.target.classList.contains("blue"))
+            text.style.color = "blue" ;
+    });
+    
+}
